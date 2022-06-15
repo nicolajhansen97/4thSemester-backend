18ed9926facd75b4329e2466acce946b58077a9e
Khorp <71438210+Khorp@users.noreply.github.com>
2022-06-15 11:29:03 +0200

    Tests

--- middleware/tests/endpoints.spec.ts
+++ middleware/tests/endpoints.spec.ts
@@ -1,33 +1,43 @@
 import { routes } from "../src/0_routes/routes";
 import request from "supertest";
+import {jest} from '@jest/globals'
 
 const api = routes;
 
-/*describe("Products, The GET with /id", () => {
-    it("GET should get a single product", async () => {
-      const result = await request(api).get("/api/products/6");
-      expect(result.body).toEqual({'no':79,'name':'flour','price':31});
-      expect(result.status).toEqual(200);
-    });
-});*/
 
-//endpoints
-/*
-  1. get all products
-  2. get product by id
-  3. post product
-  4. update product
-  5. delete product
-  6. defualt route
-*/
+//Test get all
+describe("Trees, The GET", () => {
+  it("GET all Trees", async () => {
+    const result = await request(api).get("/api/Trees/");
+    expect(result.status).toEqual(200);
+  });
+});
+//Test get with id
+describe("Trees, The GET with id", () => {
+  it("GET 1 Tree", async () => {
+    const result = await request(api).get("/api/Trees/6");
+    expect(result.body).toEqual({
+      'No': '6',
+      "TreeType": "Oak",
+      "HumidityMin": 30,
+      "HumidityMax": 40,
+      "TempMin": 20,
+      "TempMax": 30,
+      "UserId": "UserID",
+      "BarCode": "2000",
+      "__v": 0,
+      "_id": "62a9a1b7ca446cd657331bcc"
+    })
+    expect(result.status).toEqual(200);
+  });
+});
 
-//roles testing
-/*
-  admin
-  regular
-  anonymous
- */
 
-
-
+//Test update
+describe("Update a tree", () => {
+  it("Should update a tree", async () => {
+    const result = await request(api).put("/api/Trees/1");
+    expect(result.status).toEqual(200);
+  });
+});
 
